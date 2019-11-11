c859b312221999a52f49223087271947334a0bc6
Nabil Kashyap <nkashya1@swarthmore.edu>
2019-11-11 11:32:54 -0500

    finally deletes old sh file

--- arch-mdtopdf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-curr_dir=$(pwd)
-
-if [ -z $1 ]; then
-    cd ~/.bulbil/mdtopdf/ && make in_dir="$curr_dir" out_dir="$curr_dir"
-else
-    cd ~/.bulbil/mdtopdf/ && make prefix="$1" in_dir="$curr_dir" out_dir="$curr_dir"
-fi    
